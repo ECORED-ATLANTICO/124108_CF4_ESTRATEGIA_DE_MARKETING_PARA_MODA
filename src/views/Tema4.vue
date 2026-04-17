4d8e1fd81750249043efcb11bd4b42f9485a0bf8
acearaujo <159490768+acearaujo@users.noreply.github.com>
2026-04-16 21:02:37 -0500

    FIX CFA

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -18,7 +18,7 @@
 
     figure.mb-5
       .video.mb-2
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/L6_HDDHc78w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/GzboPLCn170" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Nuevas tendencias
 
 
